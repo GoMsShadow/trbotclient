bc49069059ae90b3d8758509e593d73ddc265ccb
Shadow <thor9599modric@gmail.com>
2024-11-20 16:49:48 +0900

    Add api url

--- src/api/index.js
+++ src/api/index.js
@@ -1,41 +1,43 @@
 import axios from "axios";
 
+const API_URL = "https://trbotserver.vercel.app";
+
 export const apiGetParams = async () => {
-  return await axios.get("/api/getparams");
+  return await axios.get(`${API_URL}/getparams`);
 };
 
 export const apiSetParams = async (data) => {
-  return await axios.post("/api/setparams", data);
+  return await axios.post(`${API_URL}/setparams`, data);
 };
 
 export const apiQueryOrder = async (data) => {
-  return await axios.post("/api/queryOrder", data);
+  return await axios.post(`${API_URL}/queryOrder`, data);
 };
 
 export const apiCancelOrder = async (data) => {
-  return await axios.post("/api/cancelOrder", data);
+  return await axios.post(`${API_URL}/cancelOrder`, data);
 };
 
 export const apiCancelAllOpenOrders = async () => {
-  return await axios.get("/api/cancelAllOpenOrders");
+  return await axios.get(`${API_URL}/cancelAllOpenOrders`);
 };
 
 export const apiLoadAllOrders = async () => {
-  return await axios.get("/api/loadAllOrders");
+  return await axios.get(`${API_URL}/loadAllOrders`);
 };
 
 export const apiGetSymbols = async () => {
-  return await axios.get("/api/symbol");
+  return await axios.get(`${API_URL}/symbol`);
 };
 
 export const apiCreateSymbol = async (name) => {
-  return await axios.post("/api/symbol", { name });
+  return await axios.post(`${API_URL}/symbol`, { name });
 };
 
 export const apiUpdateSymbol = async (id, name) => {
-  return await axios.put(`/api/symbol/${id}`, { name });
+  return await axios.put(`${API_URL}/symbol/${id}`, { name });
 };
 
 export const apiDeleteSymbol = async (id) => {
-  return await axios.delete(`/api/symbol/${id}`);
+  return await axios.delete(`${API_URL}/symbol/${id}`);
 };
